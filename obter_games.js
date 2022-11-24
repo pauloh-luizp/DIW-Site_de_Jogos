1120a39f389e49154dd8b84a3905d236a2430657
Paulo Henrique Luiz Pereira <paulo0147wp@outlook.com>
2022-11-23 22:29:11 -0300

    sem_sucesso_para_obter_dados_api

--- obter_games.js
+++ obter_games.js
@@ -1,3 +1,22 @@
-fetch ('https://rawg.io/api/games?search=need-for-speed-most-wanted&key=893b3e18b2ab4752a83f2cde712ccdf6')
-  .then(res => res.json ())
-  .then(data => document.getElementById('nfsmw').innerHTML = data.background_image)
+const apiKey = "893b3e18b2ab4752a83f2cde712ccdf6";
+
+function getGames(nomeJogo){
+  
+  jogo = ''
+ 
+  fetch (`https://rawg.io/api/games?search=${nomeJogo}&key=${apiKey}`)
+    .then(res => res.json ())
+    .then(data => jogo = data)
+
+  return jogo
+}
+
+
+
+exibeJogosCarrossel(jogo)
+
+document.body.onload = () => {
+  exibeJogosCarrossel();
+}
+
+
