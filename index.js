d02526a456025cfefe21d27560dd393865b37099
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2022-11-25 15:39:59 -0300

    correções iniciais

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const apiKey = "893b3e18b2ab4752a83f2cde712ccdf6";
 
-document.body.onload = () =>{
-  exibeJogosCarrossel()
-}
+window.onload = () => {
+  exibeJogosCarrossel();
+};
 
-function exibeJogosCarrossel(){
-  const jogosPrincipais = ['need-for-speed-most-wanted','gran-turismo-4','burnout-3-takendown','test-drive-unlimited']
-  let str = ''
-  var name = ''
-  var description = ''
-  var rating = ''
-  var released = ''
-  var metacritic = ''
+function exibeJogosCarrossel() {
+  const jogosPrincipais = [
+    "need-for-speed-most-wanted",
+    "gran-turismo-4",
+    "burnout-3-takendown",
+    "test-drive-unlimited",
+  ];
+
+  let str = "";
+  var name = "";
+  var description = "";
+  var rating = "";
+  var released = "";
+  var metacritic = "";
+
   //for (let i = 0; i < jogosPrincipais.length; i++){
-    fetch (`https://rawg.io/api/games/?key=${apiKey}&id=need-for-speed-most-wanted`, {mode: "no-cors"})
-    .then(res => res.json ())
-    .then(data => {
-          console.log(data.results)
-          str += `<div class="carousel-inner">
+
+  // Buscando assim funciona para trazer vários jogos
+  test = fetch(`https://rawg.io/api/games?token&key=${apiKey}`)
+    .then((res) => res.json())
+    .then((data) =>
+      console.log(data).catch((erro) => console.error("Erro:", erro))
+    );
+
+  console.log(test);
+
+  // Buscando desse jeito fica fixo apenas nesse jogo que está na URL tem que ser dinâmico
+  fetch(
+    `https://rawg.io/api/games?token&key=${apiKey}&id=need-for-speed-most-wanted`,
+    { mode: "no-cors" }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.results);
+      str += `<div class="carousel-inner">
           <div class="carousel-item active">
             <div class="row" style="margin-bottom: 35px;">
               <div class="col-xs-12 col-12 col-md-6" style="min-height: 294px;">
@@ -44,10 +65,8 @@ function exibeJogosCarrossel(){
                 </div>
               </div>
             </div>
-          </div>` 
-    } )
+          </div>`;
+    });
   //}
-  document.getElementById('jogos_de_corrida_marcantes').innerHTML = str
+  document.getElementById("jogos_de_corrida_marcantes").innerHTML = str;
 }
-
-
